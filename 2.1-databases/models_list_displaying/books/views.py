fb381f2b316dae6871dee45285bcb95f1c293bb8
Alexey Kibyakov <lexa111lexa6@mail.ru>
2023-01-30 16:44:00 +0300

    hw3 library

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,40 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from books.models import Book
+
+def convert_date(books):
+    for book in books:
+        book.pub_date = str(book.pub_date)
+    return books
+
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books = Book.objects.all()
+    convert_date(books)
+    context = {'books': books}
+    return render(request, template, context)
+
+def one_book_view(request, date):
+    template = 'books/date_list.html'
+    books = Book.objects.filter(pub_date__contains=date)
+    paginator = Paginator(books, 10)
+    page_date = request.GET.get('pub_date')
+    page = paginator.get_page(page_date)
+    prev_date = Book.objects.filter(pub_date__lt=date).values('pub_date').first()
+    next_date = Book.objects.filter(pub_date__gt=date).values('pub_date').first()
+
+    if prev_date:
+        prev_date = str(prev_date['pub_date'])
+    
+    if next_date:
+        next_date = str(next_date['pub_date'])
+
+    context = {
+        'books': books,
+        'page': page,
+        'prev_date': prev_date,
+        'next_date': next_date,
+        }
     return render(request, template, context)
