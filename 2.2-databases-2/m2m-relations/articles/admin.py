6f242292f22d7ea4b806d2af8b845005449f0be0
AlekseyKibyakov <lexa111lexa6@mail.ru>
2023-02-11 12:19:42 +0300

    hw4 t2 1/2 done

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet, ValidationError
 
-from .models import Article
+from .models import Article, Scope, Tag
 
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        for form in self.forms:
+            form.cleaned_data
+            raise ValidationError('Основным может быть только один раздел!')
+        return super().clean() 
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInline]
+    
+    
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_view = ['name']
+
+
